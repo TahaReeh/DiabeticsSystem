8a94d8f168ed6286a2ec1beac2b12379c937c29f
Taha Reeh <tahareehg@gmail.com>
2023-11-14 22:13:14 +0200

    upgrade to .net8

--- DiabeticsSystem.API/StartupExtensions.cs
+++ DiabeticsSystem.API/StartupExtensions.cs
@@ -12,7 +12,6 @@ namespace DiabeticsSystem.API
         public static WebApplication ConfigureServices(
             this WebApplicationBuilder builder)
         {
-            //AddSwagger(builder.Services);
 
             builder.Services.AddApplicationServices();
             builder.Services.AddPersistenceServices(builder.Configuration);
@@ -69,51 +68,6 @@ namespace DiabeticsSystem.API
                 logger.LogError(ex, "An error occurred while migrating the database.");
             }
         }
-
-        //private static void AddSwagger(IServiceCollection services)
-        //{
-        //    services.AddSwaggerGen(c =>
-        //    {
-        //        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        //        {
-        //            Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-        //              Enter 'Bearer' [space] and then your token in the text input below.
-        //              \r\n\r\nExample: 'Bearer 12345abcdef'",
-        //            Name = "Authorization",
-        //            In = ParameterLocation.Header,
-        //            Type = SecuritySchemeType.ApiKey,
-        //            Scheme = "Bearer"
-        //        });
-
-        //        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        //          {
-        //            {
-        //              new OpenApiSecurityScheme
-        //              {
-        //                Reference = new OpenApiReference
-        //                  {
-        //                    Type = ReferenceType.SecurityScheme,
-        //                    Id = "Bearer"
-        //                  },
-        //                  Scheme = "oauth2",
-        //                  Name = "Bearer",
-        //                  In = ParameterLocation.Header,
-
-        //                },
-        //                new List<string>()
-        //              }
-        //            });
-
-        //        c.SwaggerDoc("v1", new OpenApiInfo
-        //        {
-        //            Version = "v1",
-        //            Title = "Diabetics System API",
-
-        //        });
-
-        //        //c.OperationFilter<FileResultContentTypeOperationFilter>();
-        //    });
-        //}
 
     }
 }
