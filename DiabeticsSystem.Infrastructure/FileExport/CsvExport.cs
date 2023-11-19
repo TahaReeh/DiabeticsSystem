8b52595b5902d4ac5224017b0355e51b6929eff7
Taha Reeh <tahareehg@gmail.com>
2023-11-19 02:23:55 +0200

    edited CsvWriter cultureInfo + encoding

--- DiabeticsSystem.Infrastructure/FileExport/CsvExport.cs
+++ DiabeticsSystem.Infrastructure/FileExport/CsvExport.cs
@@ -2,6 +2,7 @@
 using DiabeticsSystem.Application.Contracts.Infrastructure;
 using DiabeticsSystem.Application.Features.PatientMovements.Queries.GetPatientMovmentExport;
 using System.Globalization;
+using System.Text;
 
 namespace DiabeticsSystem.Infrastructure.FileExport
 {
@@ -10,9 +11,9 @@ namespace DiabeticsSystem.Infrastructure.FileExport
         public byte[] ExportPatientMovementToCsv(List<PatientMovementExportDTO> exportDTOs)
         {
             using var memoryStream = new MemoryStream();
-            using (var writer = new StreamWriter(memoryStream))
+            using (var writer = new StreamWriter(memoryStream, Encoding.UTF8))
             {
-                using var csvWriter = new CsvWriter(writer,CultureInfo.CurrentCulture);
+                using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
                 csvWriter.WriteRecords(exportDTOs);
             }
 
